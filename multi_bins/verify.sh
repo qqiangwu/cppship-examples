0bbe94ce5af2f87337a8351b9216ca94de28e2a6
qqiangwu <wqzhiep@gmail.com>
2023-04-21 20:26:29 +0800

    feat: add test for -q

--- multi_bins/verify.sh
+++ multi_bins/verify.sh
@@ -6,6 +6,6 @@ test -x build/debug/multi_bins
 test -x build/debug/bin1
 test -x build/debug/bin2
 
-test "$(cppship run --bin bin1 | tail -n1)" = "bin1"
-test "$(cppship run --bin bin2 | tail -n1)" = "bin2"
-test "$(cppship run --bin multi_bins | tail -n1)" = "main"
+test "$(cppship run -q --bin bin1)" = "bin1"
+test "$(cppship run -q --bin bin2)" = "bin2"
+test "$(cppship run -q --bin multi_bins)" = "main"
