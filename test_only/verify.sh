c34182046e784a279c96bf08237a008e645ebbda
qqiangwu <wqzhiep@gmail.com>
2023-04-26 20:34:03 +0800

    feat: add more for cppship test options

--- test_only/verify.sh
+++ test_only/verify.sh
@@ -11,3 +11,11 @@ test -x build/debug/a_test
 rm -rf build
 cppship build --tests
 test -x build/debug/a_test
+
+rm -rf build
+cppship test c | grep 'test `c` not found'
+cppship test -q b1 | grep "b1"
+
+out=$(cppship test -q -R b)
+echo $out | grep "b1_test.*Passed"
+echo $out | grep "b2_test.*Passed"
