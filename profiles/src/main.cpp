976e1576d4c6175be306531cfa8a2d0c53992103
qqiangwu <wqzhiep@gmail.com>
2023-04-28 00:04:43 +0800

    feat: add test for profile config

---
+++ profiles/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+#ifdef C
+        std::cout << A;
+#endif
+
+#ifdef D
+        std::cout << B;
+#endif
+}
---
+++ profiles/verify.sh
@@ -0,0 +1,2 @@
+test $(cppship run -q) = "1"
+test $(cppship run -q -r) = "2"
